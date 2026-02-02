763bfff37ba94c2c147b2ec7179a9de66773f6f7
Nikola Jovanovic <jovanovicn.96@gmail.com>
2026-02-02 21:27:38 +0100

    main cleanup

--- lorenz_attractor/lorenz.c
+++ lorenz_attractor/lorenz.c
@@ -15,20 +15,29 @@
 #define KEY_S 83
 
 #define PATH_LENGTH_SHIFT 5
+/* Length of the path history array.
+   Chosen to be a power of two so the modulo operation
+   can be optimized to a bitwise AND.*/
 #define PATH_LENGTH (1l << PATH_LENGTH_SHIFT)
 
-#define IGNORE_BUTTON_TICKS 15
+#define IGNORE_BUTTON_COOLDOWN 15
 
+// Fixed point arithmetic macros
+
+/* Number of simulated decimal places.
+   This number should be as large as possible,
+   but such that it never causes overflow */
 #define FP_SHIFT 8
 #define TO_FIXED(x) ((int32_t)((x) * (1l << FP_SHIFT)))
 #define FROM_FIXED(x) ((x) >> FP_SHIFT)
+/* Multiplication of two fixed point numbers results in a double
+   shifted value, so it needs to be shifted one step back */
 #define FIXED_MUL(a, b) (FROM_FIXED(((a) * (b))))
 
 // TODO:
 // - Add visit count to path history so the pixel deletion is correct
 // - Use different chars for path history fading
 // - Use dot instead of char resolution
-
 
 /* Marks the camera view angle of the system */
 enum view_axis {
@@ -45,16 +54,63 @@ enum reset_erase_mode {
     NO_ERASE, ERASE
 };
 
-// Path history variables, globals to preserve stack space
+// Path history tracking variables
+
+/* History of the last PATH_LENGTH X screen coordinates */
 int positions_x[PATH_LENGTH];
+/* History of the last PATH_LENGTH Y screen coordinates */
 int positions_y[PATH_LENGTH];
+/* Index of the current position in revolving path history arrays */
 int path_index = 0;
+/* Helper variable containing the index of the oldest position in path history */
 int oldest_path_index = 0;
 
 // Display stats
+
+/* Stats visiblity flag */
 enum stats_visibility print_stats = STATS_OFF;
-int ignore_button_ticks = 0;
+/* Cooldown required between registering button presses */
+unsigned char ignore_button_cooldown = 0;
+/* Latest pressed button character */
+unsigned char char_input;
+/* Simulation iteration counter */
 uint32_t iteration = 1;
+
+// Lorenz attractor parameters
+// Parameters are hardcoded to values that produce a chaotic behavior
+
+/* The first of the Lorenz attractor parameters */
+int32_t ro = TO_FIXED(28.0);
+/* The second of the Lorenz attractor parameters */
+int32_t sigma = TO_FIXED(10.0);
+/* The third of the Lorenz attractor parameters */
+int32_t beta = TO_FIXED(8.0 / 3.0);
+/* Simulation time step */
+int32_t dt = TO_FIXED(0.01);
+
+// System state variables
+
+/* The X coordinate of the system */
+int32_t x = TO_FIXED(1.0);
+/* The Y coordinate of the system */
+int32_t y = TO_FIXED(1.0);
+/* The Z coordinate of the system */
+int32_t z = TO_FIXED(1.0);
+/* Current camera projection */
+enum view_axis projection = ASIS_XZ;
+
+// Simulation helper variables
+
+/* Change in X coordinate in the current step */
+int32_t dx = 0;
+/* Change in Y coordinate in the current step */
+int32_t dy = 0;
+/* Change in Z coordinate in the current step */
+int32_t dz = 0;
+/* Screen X coordinate of the system */
+int screen_x = SCREEN_WIDTH_HALF;
+/* Screen Y coordinate of the system */
+int screen_y = SCREEN_HEIGHT_HALF;
 
 /* Print welcome screen */
 void print_welcome_screen()
@@ -237,45 +293,67 @@ void reset_path_history(enum reset_erase_mode erase_mode)
     path_index = 0;
 }
 
+/* Handles user input actions */
+void handle_user_input()
+{
+    if (ignore_button_cooldown == 0)
+    {
+        char_input = getk();
+        switch (char_input)
+        {
+        case KEY_1:
+            if (projection != ASIS_XY)
+            {
+                projection = ASIS_XY;
+                reset_path_history(ERASE);
+            }
+            ignore_button_cooldown = IGNORE_BUTTON_COOLDOWN;
+            break;
+        case KEY_2:
+            if (projection != ASIS_XZ)
+            {
+                projection = ASIS_XZ;
+                reset_path_history(ERASE);
+            }
+            ignore_button_cooldown = IGNORE_BUTTON_COOLDOWN;
+            break;
+        case KEY_3:
+            if (projection != ASIS_YZ)
+            {
+                projection = ASIS_YZ;
+                reset_path_history(ERASE);
+            }
+            ignore_button_cooldown = IGNORE_BUTTON_COOLDOWN;
+            break;
+        case KEY_S:
+            toggle_stats();
+            ignore_button_cooldown = IGNORE_BUTTON_COOLDOWN;
+            break;
+        default:
+            break;
+        }
+    }
+    else
+    {
+        ignore_button_cooldown--;
+    }
+}
+
 int main()
 {
     print_welcome_screen();
 
     // Wait for Enter key
-    unsigned char char_input;
     do {
         char_input = fgetc_cons();
     } while (char_input != KEY_ENTER);
 
     clear_welcome_screen();
 
-    // Lorenz attractor parameters
-    int32_t ro = TO_FIXED(28.0);
-    int32_t sigma = TO_FIXED(10.0);
-    int32_t beta = TO_FIXED(8.0 / 3.0);
-
-    // Initial conditions
-    int32_t x = TO_FIXED(1.0);
-    int32_t y = TO_FIXED(1.0);
-    int32_t z = TO_FIXED(1.0);
-
     // Path history without erasing, because the arrays are uninitialized
     reset_path_history(NO_ERASE);
 
-    // Time step
-    int32_t dt = TO_FIXED(0.01);
-
-    // Projection axis
-    enum view_axis projection = ASIS_XZ;
-
-    // Loop variable initialization
-    int32_t dx = 0;
-    int32_t dy = 0;
-    int32_t dz = 0;
-    int screen_x = SCREEN_WIDTH_HALF;
-    int screen_y = SCREEN_HEIGHT_HALF;
-
-ITER:
+SIM_ITER:
     // Compute derivatives
     dx = FIXED_MUL(sigma, (y - x));
     dy = FIXED_MUL(x, (ro - z)) - y;
@@ -286,7 +364,7 @@ ITER:
     y += FIXED_MUL(dy, dt);
     z += FIXED_MUL(dz, dt);
 
-    // Map to screen coordinates
+    // Map state to screen coordinates
     switch (projection)
     {
         case ASIS_XY:
@@ -303,33 +381,32 @@ ITER:
             break;
     }
 
-    // Plot point if within bounds
+    // Update the screen and path history if the point is within screen bounds
+    // and has moved since the last iteration
     if (screen_x >= 1 && screen_x < SCREEN_WIDTH - 1 &&
-        screen_y >= 1 && screen_y < SCREEN_HEIGHT - 1)
+        screen_y >= 1 && screen_y < SCREEN_HEIGHT - 1 &&
+        (screen_x != positions_x[path_index] || screen_y != positions_y[path_index]))
     {
-        if (screen_x != positions_x[path_index] || screen_y != positions_y[path_index])
+        // Erase oldest point in path history
+        oldest_path_index = (path_index + 1) & (PATH_LENGTH - 1);
+        if (positions_x[oldest_path_index] != -1 && positions_y[oldest_path_index] != -1)
         {
-            // Erase oldest point in path history
-            oldest_path_index = (path_index + 1) & (PATH_LENGTH - 1);
-            if (positions_x[oldest_path_index] != -1 && positions_y[oldest_path_index] != -1)
-            {
-                gal_gotoxy(positions_x[oldest_path_index], positions_y[oldest_path_index]);
-                gal_putc(' ');
-            }
-
-            // Move to the next position in the path history
-            path_index = oldest_path_index;
-
-            // Store new position in history
-            positions_x[path_index] = screen_x;
-            positions_y[path_index] = screen_y;
+            gal_gotoxy(positions_x[oldest_path_index], positions_y[oldest_path_index]);
+            gal_putc(' ');
         }
+
+        // Move to the next position in the path history
+        path_index = oldest_path_index;
+
+        // Store new position in history
+        positions_x[path_index] = screen_x;
+        positions_y[path_index] = screen_y;
 
         gal_gotoxy(screen_x, screen_y);
         gal_putc('#');
     }
 
-    // Update iteration count
+    // Update stats display if enabled
     if (print_stats == STATS_ON)
     {
         gal_gotoxy(4, SCREEN_HEIGHT - 5);
@@ -345,53 +422,10 @@ ITER:
         print_int_6((int)iteration);
     }
 
-    if (ignore_button_ticks == 0)
-    {
-        char_input = getk();
-        switch (char_input)
-        {
-        case KEY_1:
-            if (projection != ASIS_XY)
-            {
-                projection = ASIS_XY;
-                reset_path_history(ERASE);
-            }
-            ignore_button_ticks = IGNORE_BUTTON_TICKS;
-            break;
-        case KEY_2:
-            if (projection != ASIS_XZ)
-            {
-                projection = ASIS_XZ;
-                reset_path_history(ERASE);
-            }
-            ignore_button_ticks = IGNORE_BUTTON_TICKS;
-            break;
-        case KEY_3:
-
-            if (projection != ASIS_YZ)
-            {
-                projection = ASIS_YZ;
-                reset_path_history(ERASE);
-            }
-            ignore_button_ticks = IGNORE_BUTTON_TICKS;
-            break;
-        case KEY_S:
-            toggle_stats();
-            ignore_button_ticks = IGNORE_BUTTON_TICKS;
-            break;
-        default:
-            break;
-        }
-    }
-
-    // Decrease the button press cooldown
-    if (ignore_button_ticks > 0)
-    {
-        ignore_button_ticks--;
-    }
+    handle_user_input();
 
     iteration++;
-    goto ITER;
+    goto SIM_ITER;
 
     return 0;
 }
